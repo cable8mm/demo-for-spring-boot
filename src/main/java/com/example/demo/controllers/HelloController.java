12acfe5726e968bb23685b220bd04e4abc2d486c
Samgu Lee <cable8mm@gmail.com>
2024-02-25 21:06:45 +1300

    Install spring boot devtools (#4)
    * Install spring boot devtools
    
    * Modify README and add .editorconfig
    
    * Add welcome page
    
    * Install thymeleaf and add hello template
    
    * Move hello method to HelloController

---
+++ src/main/java/com/example/demo/controllers/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(Model model) {
+
+        model.addAttribute("name", "You");
+
+        return "hello";
+    }
+
+}
